0484d34dd604d4a17b2f06447314980a5a6777a4
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2021-12-26 23:57:44 -0500

    finalize README and updated the rest of localstorage

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Scheduler
+# Work Day Scheduler
+
+## Discription
+
+For this assignment/challenge I was asked to create a simple calendar application that allows a user to save events for each hour of the day with each time block that is color-coded to indicate whether it is in the past, present or future. 
+
+## Acceptance Criteria
+
+<img width="598" alt="Screen Shot 2021-12-26 at 11 50 32 PM" src="https://user-images.githubusercontent.com/92699311/147435677-95284539-7f5f-492a-b70f-74de8c1d23b6.png">
+
+
+## Application Image
+
+![Screen Shot 2021-12-26 at 11 03 57 PM](https://user-images.githubusercontent.com/92699311/147435700-0fc37d1b-dd9a-47a3-ae7b-065e0472f885.png)
+
+
+## Built With
+
+* jQuery
+* Bootstrap
+* CSS
+* HTML
+
+## Website
+
+[Joseph's Work Day Scheduler](https://jmcampbell2021.github.io/scheduler/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     var currentDate = $('#currentDay');
     currentDate.text(currentTime);
 
-    var date = new Date(); // init date and time
-    var currentHour = date.getHours(); // returns 0-23 on a 24 hour clock
+    var date = new Date();
+    var currentHour = date.getHours();
 
     for (var i = 8; i < 19; i++) {
         if (i < currentHour) {
@@ -17,11 +17,54 @@ $(document).ready(function () {
         }
     }
 
-    $('.saveBtn').click(function(){
+    $('.saveBtn').click(function(event){
+        event.preventDefault();
+
         var Task8 = document.getElementById('8').value;
-        localStorage.setItem('Task', Task8);
+        localStorage.setItem('Task8', Task8);
+        var Task9 = document.getElementById('9').value;
+        localStorage.setItem('Task9', Task9);
+        var Task10 = document.getElementById('10').value;
+        localStorage.setItem('Task10', Task10);
+        var Task11 = document.getElementById('11').value;
+        localStorage.setItem('Task11', Task11);
+        var Task12 = document.getElementById('12').value;
+        localStorage.setItem('Task12', Task12);
+        var Task13 = document.getElementById('13').value;
+        localStorage.setItem('Task13', Task13);
+        var Task14 = document.getElementById('14').value;
+        localStorage.setItem('Task14', Task14);
+        var Task15 = document.getElementById('15').value;
+        localStorage.setItem('Task15', Task15);
+        var Task16 = document.getElementById('16').value;
+        localStorage.setItem('Task16', Task16);
+        var Task17 = document.getElementById('17').value;
+        localStorage.setItem('Task17', Task17);
+        var Task18 = document.getElementById('18').value;
+        localStorage.setItem('Task18', Task18);
     })
 
-    var loadTask8 = localStorage.getItem('Task');
-    document.getElementById('8').value = loadTask8
+    var loadTask8 = localStorage.getItem('Task8');
+    document.getElementById('8').value = loadTask8;
+    var loadTask9 = localStorage.getItem('Task9');
+    document.getElementById('9').value = loadTask9
+    var loadTask10 = localStorage.getItem('Task10');
+    document.getElementById('10').value = loadTask10
+    var loadTask11 = localStorage.getItem('Task11');
+    document.getElementById('11').value = loadTask11
+    var loadTask12 = localStorage.getItem('Task12');
+    document.getElementById('12').value = loadTask12
+    var loadTask13 = localStorage.getItem('Task13');
+    document.getElementById('13').value = loadTask13
+    var loadTask14 = localStorage.getItem('Task14');
+    document.getElementById('14').value = loadTask14
+    var loadTask15 = localStorage.getItem('Task15');
+    document.getElementById('15').value = loadTask15
+    var loadTask16 = localStorage.getItem('Task16');
+    document.getElementById('16').value = loadTask16
+    var loadTask17 = localStorage.getItem('Task17');
+    document.getElementById('17').value = loadTask17
+    var loadTask18 = localStorage.getItem('Task18');
+    document.getElementById('18').value = loadTask18
+
 });
